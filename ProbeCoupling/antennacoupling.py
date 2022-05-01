118708e8b2b99221d40d71b1d4750aa2d6ce1a4d
rajapali <22496421@student.uwa.edu.au>
2022-05-01 23:56:15 +0800

    read file and plot raw data

--- ProbeCoupling/antennacoupling.py
+++ ProbeCoupling/antennacoupling.py
@@ -1,6 +1,20 @@
 
 import gather_data as gd
+import cryolib.general as gen
 
-params = gd.input_popup()
-gd.gather_data(params, True)
+
+#params = gd.input_popup()
+#gd.gather_data(params, True)
+
+
+
+path = r"C:\Users\deepa\PycharmProjects\pythonProject\Deepali_single_sweep_010.hdf5"
+freq, vna, param = gd.read_h5_file(path)
+db_data = gen.complex_to_dB(vna)
+
+
+gd.present_data(db_data, freq, param[0], param[1])
+
+
+
 
